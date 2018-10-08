4e46f29519e320dbd9d29023218a5a00b41536e3
Conbreak <juan.mason@hotmail.com>
2018-10-08 00:20:20 -0300

    Preprocesamiento_de_los_datos
    Exploración de valores faltantes. Hay 15 en la variable 'B - V' de Hipparcos.

--- 02.Preprocesamiento/mapeo_hyades.r
+++ 02.Preprocesamiento/mapeo_hyades.r
@@ -1,9 +1,9 @@
 ## Función para llamar al Dataframe final de Hipparcos ##
 
 mapeo_hyades <- function(){
-  
-  hip <- read_excel("./00.Datos_y_TP/hyades_source.xlsx", sheet = 2)
-  seleccion <- read_csv("./00.Datos_y_TP/Hyades_seleccion.csv")
+  setwd(getwd())
+  hip <- read_excel("../00.Datos_y_TP/hyades_source.xlsx", sheet = 2)
+  seleccion <- read_csv("../00.Datos_y_TP/Hyades_seleccion.csv")
   
   hip <- hip %>%
     mutate(Symbad_Hyades = (.$HIP %in% seleccion$cercanaHip))
