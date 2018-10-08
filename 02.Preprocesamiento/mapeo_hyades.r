59b713974bf099d13bdd9642058a90cdc70d4920
Conbreak <juan.mason@hotmail.com>
2018-10-07 22:16:32 -0300

    Script_Hip_final
    Script para llamar al dataframe final.

---
+++ 02.Preprocesamiento/mapeo_hyades.r
@@ -0,0 +1,12 @@
+## Función para llamar al Dataframe final de Hipparcos ##
+
+mapeo_hyades <- function(){
+  
+  hip <- read_excel("./00.Datos_y_TP/hyades_source.xlsx", sheet = 2)
+  seleccion <- read_csv("./00.Datos_y_TP/Hyades_seleccion.csv")
+  
+  hip <- hip %>%
+    mutate(Symbad_Hyades = (.$HIP %in% seleccion$cercanaHip))
+
+return(hip)}
+
